5876ec9f07d7d379c25cb651df5fc702f05795b3
Beshoy Fady <beshoyfadykamel1@gmil.com>
2026-03-08 17:31:14 +0200

    feat: add profile request validation and resource

---
+++ app/Http/Resources/Users/ProfileResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Users;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
