3b0418b2389f6b3b64cd87c854638c1bb50b9fae
Beshoy Fady <beshoyfadykamel1@gmil.com>
2026-03-08 21:23:18 +0200

    refactor: update users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('status', ['1', '0'])->default('1');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
